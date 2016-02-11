fb57d4ab126be2c2305aeb16d2f81a171dc4245e
nolanreilly <nolanreilly@yahoo.com>
2016-02-10 17:42:08 -0800

    finished warmup
    removal of elements in vector and list

---
+++ l_removeOdd.cpp
@@ -0,0 +1,37 @@
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+// Remove the odd integers from li.
+// It is acceptable if the order of the remaining even integers is not
+// the same as in the original list.
+void removeOdds(list<int>& li)
+{
+    for(auto p = li.begin(); p != li.end(); p++) {
+        if(*p % 2 != 0)
+            li.erase(p);
+    }
+}
+
+void test()
+{
+    int a[8] = { 2, 8, 5, 6, 7, 3, 4, 1 };
+    list<int> x(a, a+8);  // construct x from the array
+    assert(x.size() == 8 && x.front() == 2 && x.back() == 1);
+    removeOdds(x);
+    assert(x.size() == 4);
+    vector<int> v(x.begin(), x.end());  // construct v from x
+    sort(v.begin(), v.end());
+    int expect[4] = { 2, 4, 6, 8 };
+    for (int k = 0; k < 4; k++)
+        assert(v[k] == expect[k]);
+}
+
+int main()
+{
+    test();
+    cout << "Passed" << endl;
+}
